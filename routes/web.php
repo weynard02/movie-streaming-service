9672383a11407e5e88c7b947db12905a05d2b14a
richiesuper <richie9963@gmail.com>
2023-03-27 17:55:10 +0700

    refactor: add other endpoints

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MyController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ArtistController;
 use App\Http\Controllers\GenreController;
+use App\Http\Controllers\LoginController;
+use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,15 +20,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomeController::class);
 
-//route awal sementara
-//ke depannya, menggunakan perantara controller
-Route::get('/my', function () {
-    return view('dashboard.index');
-});
+Route::get('/my', MyController::class);
+
+Route::get('/login', LoginController::class);
+
 
 Route::get('/admin', [AdminController::class, 'index']);
 Route::get('/admin/create', [AdminController::class, 'create']);
