4f8b380312cd20094db1de3927c991794a54ee3f
richiesuper <richie9963@gmail.com>
2023-03-27 17:50:01 +0700

    feat: add resources/views/my/index.blade.php

---
+++ resources/views/my/index.blade.php
@@ -0,0 +1,16 @@
+@extends('master')
+@section('content')
+<div class="card text-center">
+  <div class="card-header">
+    Disney Minus Coldmoon
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Coming Soon</h5>
+    <p class="card-text">FP CAMIN ALPRO 2023</p>
+    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="btn btn-primary">Go somewhere</a>
+  </div>
+  <div class="card-footer text-muted">
+    2 days ago
+  </div>
+</div>
+@endsection
