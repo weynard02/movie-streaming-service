f77883f06229d706b899d435fb249dcb4a9a16ac
richiesuper <richie9963@gmail.com>
2023-03-28 21:43:28 +0700

    fix: anchor in navbar to watchlist page

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -11,7 +11,7 @@
                     <a class="nav-link active" aria-current="page" href="/">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Watchlist</a>
+                    <a class="nav-link" href="/watchlist">Watchlist</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href='/movie'>Movie List</a>
