d7b4f71328cbf7008620186e24acecdee1c2a5fc
richiesuper <richie9963@gmail.com>
2023-03-28 21:41:52 +0700

    feat: implement creating and displaying watchlist

--- resources/views/watchlist/create.blade.php
+++ resources/views/watchlist/create.blade.php
@@ -1,35 +1,24 @@
 @extends('master')
 @section('content')
-    <h1 align=center> Add Playlist</h1>
+    <h1 align=center> Add Movie</h1>
     <div class="card-body">
         <form action="/watchlist" method="post" enctype="multipart/form-data">
             @csrf
             <p class="card-text">
             <div class="row">
                 <ul class="list-group">
-                    <h2>Movies:</h2>
-                    <li class="list-group-item d-flex justify-content-between align-items-start">
-                        <select class="form-select form-control" name="genre">
-                            <option selected value="">-</option>
-                            @foreach ($movies as $movie)
-                                <option value="{{ $movie->id }}">{{ $movie->name }}</option>
-                            @endforeach
-                        </select>
-                        <form action='/watchlist/movie/{{ $movie->id }}' method="post" enctype="multipart/form-data">
-                            @csrf
-                            @method('delete')
-                            <button type="submit" class="btn btn-danger btn-sm">Remove</button>
-                        </form>
-                    </li>
-                    <li class="list-group-item">
-                        <div class="ms-2 me-auto">
-                            <a href="/watchlist/movie/{{ $movie->id }}" class="btn btn-outline-primary">&#43; Add
-                                Movie</a>
-                        </div>
-                    </li>
+                    <h2>Movie to Add:</h2>
+                    <select class="form-select form-control" name="movie_id">
+                        <option selected value="">Select Movie</option>
+                        @foreach ($movies as $movie)
+                            <option value="{{ $movie->id }}">{{ $movie->name }}</option>
+                        @endforeach
+                    </select>
                 </ul>
             </div>
             </p>
+            <a href="./" class="btn btn-danger">&lt; Back</a>
+            <button type="submit" class="btn btn-success">Submit</button>
         </form>
     </div>
 @endsection
--- resources/views/watchlist/index.blade.php
+++ resources/views/watchlist/index.blade.php
@@ -6,6 +6,17 @@
                 {{ session('pesan') }}
             </div>
         @endif
+        <div class="row my-5">
+            <div class="col-6 d-flex justify-content-center">
+                <a class="btn btn-primary" href="/watchlist/create"> Add to Watchlist </a>
+            </div>
+            <div class="col-6 d-flex justify-content-center">
+                <a class="btn btn-primary" href="/watchlist/edit"> Edit Watchlist </a>
+            </div>
+        </div>
+        <div class="row my-5">
+
+        </div>
         <div class="header my-5 py-2 bg-info">
             <h2 class="text-dark">{{ $user->name }}'s Watchlist</h2>
         </div>
@@ -13,7 +24,7 @@
             <p class="card-text">
             <div class="row">
                 <ul class="list-group">
-                    @foreach ($watchlist as $movie)
+                    @foreach ($movies as $movie)
                         <li class="list-group-item d-flex justify-content-between align-items-start">
                             <div class="ms-2 me-auto">
                                 {{ $movie->name }}
