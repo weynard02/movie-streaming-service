d1dcb4a2412dbda206a80ff8cce007a6cd05a2f4
richiesuper <richie9963@gmail.com>
2023-03-28 23:01:59 +0700

    feat: change add button placement in watchlist view

--- resources/views/watchlist/index.blade.php
+++ resources/views/watchlist/index.blade.php
@@ -6,13 +6,13 @@
                 {{ session('pesan') }}
             </div>
         @endif
-        <div class="row my-5">
-            <div class="d-flex justify-content-center">
-                <a class="btn btn-primary" href="/watchlist/create"> Add to Watchlist </a>
-            </div>
-        </div>
         <div class="header my-5 py-2 bg-info rounded bg-opacity-25">
             <h2 class="text-dark ms-4">{{ $user->name }}'s Watchlist</h2>
+        </div>
+        <div class="row my-5">
+            <div class="d-flex justify-content-start">
+                <a class="btn btn-primary" href="/watchlist/create"> Add to Watchlist </a>
+            </div>
         </div>
         <div class="card-body">
             <p class="card-text">
